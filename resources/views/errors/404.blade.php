c52c796c45dbb457a115c0b69407feb76620056f
Raphael Moraes <antnraphael@gmail.com>
2020-02-07 23:32:27 -0300

    ALteração na página de 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,4 @@
-@extends('adminlte::page')
-@section('content_header')@stop
 
-@section('content')
     <div class="row page-error">
        <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-md-center">
             <div class="error-code">404</div>
@@ -10,4 +7,4 @@
         <div class="error-description">Página não encontrada</div>
        </div>
     </div>
-@stop
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
